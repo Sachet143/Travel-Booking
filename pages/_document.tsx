c1c1ebdba20903940e2897904b6e859e3a3b22a1
lokesh bajracharya <lokesh.bajracharya55@gmail.com>
2022-08-10 01:11:19 +0545

    added bootstrap

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 class CustomDocument extends Document {
     render() {
@@ -13,10 +14,13 @@ class CustomDocument extends Document {
                     />
                     <link rel='apple-touch-icon' sizes='180x180' href='/images/apple-touch-icon.png' />
                     <link rel='shortcut icon' href='/images/favicon.png' />
+                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous" />
                 </Head>
                 <body>
                     <Main />
                     <NextScript />
+                    <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+                        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossOrigin="anonymous" />
                 </body>
             </Html>
         )
