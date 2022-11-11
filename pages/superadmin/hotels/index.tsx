e34f95d3d299dd772fcb88097abae04117ec1111
lokeshbaj <lokesh@coveredpress.com>
2022-11-12 01:47:21 +0545

    superadmin hotel listing

--- pages/superadmin/hotels/index.tsx
+++ pages/superadmin/hotels/index.tsx
@@ -1,66 +1,36 @@
 import SuperadminLayout from '@/components/layout/superadmin';
+import { cleanUrlParams, renderLocation } from '@/services/helper';
 import { SearchOutlined } from '@ant-design/icons';
-import type { InputRef } from 'antd';
+import { InputRef, Skeleton } from 'antd';
 import { Button, Input, Space, Table } from 'antd';
 import type { ColumnsType, ColumnType } from 'antd/es/table';
 import type { FilterConfirmProps } from 'antd/es/table/interface';
 import React, { ReactElement, useRef, useState } from 'react';
 import Highlighter from 'react-highlight-words';
+import useSWR from 'swr';
 
 interface DataType {
-  hotel_name: string;
-  category: string;
+  name: string;
   location: string;
   admin: string;
-  status: string;
+  phone: string;
+  category: string;
   action: ReactElement;
-  // key: string;
-  // name: string;
-  // age: number;
-  // address: string;
 }
 
 type DataIndex = keyof DataType;
 
-const data: DataType[] = [
-  {
-    hotel_name: "Shangri-la",
-    category: "Hotel",
-    location: "Lazimpat, Kathmandu, Nepal",
-    admin: "Lokesh Bajracharya",
-    status: "true",
-    action: <Button className="btn btn-admin-dark">Login</Button>
-  },
-  {
-    hotel_name: "Shangri-la",
-    category: "Hotel",
-    location: "Lazimpat, Kathmandu, Nepal",
-    admin: "Lokesh Bajracharya",
-    status: "true",
-    action: <Button className="btn btn-admin-dark">Login</Button>
-  },
-  {
-    hotel_name: "Shangri-la",
-    category: "Hotel",
-    location: "Lazimpat, Kathmandu, Nepal",
-    admin: "Lokesh Bajracharya",
-    status: "true",
-    action: <Button className="btn btn-admin-dark">Login</Button>
-  },
-  {
-    hotel_name: "Shangri-la",
-    category: "Hotel",
-    location: "Lazimpat, Kathmandu, Nepal",
-    admin: "Lokesh Bajracharya",
-    status: "true",
-    action: <Button className="btn btn-admin-dark">Login</Button>
-  },
-];
-
 const App: React.FC = () => {
+
   const [searchText, setSearchText] = useState('');
   const [searchedColumn, setSearchedColumn] = useState('');
   const searchInput = useRef<InputRef>(null);
+
+  const [page, setPage] = useState(1);
+  const { data, error } = useSWR(cleanUrlParams(`admin/hotel`, {
+    [searchedColumn === "name" ? "search" : "whole_location"]: searchText,
+    page,
+  }));
 
   const handleSearch = (
     selectedKeys: string[],
@@ -76,6 +46,8 @@ const App: React.FC = () => {
     clearFilters();
     setSearchText('');
   };
+
+
 
   const getColumnSearchProps = (dataIndex: DataIndex): ColumnType<DataType> => ({
     filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
@@ -148,21 +120,12 @@ const App: React.FC = () => {
   const columns: ColumnsType<DataType> = [
     {
       title: 'Name',
-      dataIndex: 'hotel_name',
-      key: 'hotel_name',
+      dataIndex: 'name',
+      key: 'name',
       width: '20%',
-      sorter: (a, b) => a.hotel_name.length - b.hotel_name.length,
+      sorter: (a, b) => a.name.length - b.name.length,
       sortDirections: ['descend', 'ascend'],
-      ...getColumnSearchProps('hotel_name'),
-    },
-    {
-      title: 'Category',
-      dataIndex: 'category',
-      key: 'category',
-      width: '20%',
-      sorter: (a, b) => a.category.length - b.category.length,
-      sortDirections: ['descend', 'ascend'],
-      ...getColumnSearchProps('category'),
+      ...getColumnSearchProps('name'),
     },
     {
       title: 'Location',
@@ -180,25 +143,54 @@ const App: React.FC = () => {
       width: '20%',
       sorter: (a, b) => a.admin.length - b.admin.length,
       sortDirections: ['descend', 'ascend'],
-      ...getColumnSearchProps('admin'),
+      // ...getColumnSearchProps('admin'),
     },
     {
-      title: 'Status',
-      dataIndex: 'status',
-      key: 'status',
-      ...getColumnSearchProps('status'),
+      title: 'Phone',
+      dataIndex: 'phone',
+      key: 'phone',
+      width: '20%'
     },
     {
-      title: 'Action',
-      dataIndex: 'action',
-      key: 'action',
-      ...getColumnSearchProps('action'),
+      title: 'Category',
+      dataIndex: 'category',
+      key: 'category',
+      width: '20%',
+      sorter: (a, b) => a.category.length - b.category.length,
+      sortDirections: ['descend', 'ascend'],
+      // ...getColumnSearchProps('category'),
     },
+    // {
+    //   title: 'Action',
+    //   dataIndex: 'action',
+    //   key: 'action',
+    //   ...getColumnSearchProps('action'),
+    // },
   ];
 
   return (
     <SuperadminLayout title="Hotels">
-      <Table columns={columns} dataSource={data} pagination={{ pageSize: 6, total: 10 }} />
+      {
+        !data && !error ?
+          <Skeleton active />
+          :
+          <>
+            <Table columns={columns} dataSource={data?.data?.map((hotel: any) => ({
+              name: hotel.name,
+              location: renderLocation({ city: hotel.location.city, state: hotel.location.state, country: hotel.location.country }),
+              admin: hotel.user.name,
+              phone: hotel.user.phone || '-',
+              category: hotel.category.title
+            }))}
+              pagination={{
+                current: data?.current_page,
+                pageSize: data?.per_page,
+                total: data?.total,
+                onChange: setPage
+              }}
+            />
+          </>
+      }
     </SuperadminLayout>
 
   );
