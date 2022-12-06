b7ae31ded161c04b5efaf75133daa352e4684abd
Prawesh Lamsal <prawesh.lamsal@infodevelopers.com.np>
2022-12-06 23:46:49 +0545

    Updated items

--- components/client/RoomTable.tsx
+++ components/client/RoomTable.tsx
@@ -25,68 +25,77 @@ const RoomTable = ({ roomLoading, rooms }: any) => {
   const [selectedRooms, setSelectedRooms] = useState<any>([]);
 
   function handleRoomChange(room: any, roomCount: any) {
-    setSelectedRooms([...selectedRooms, { ...room, roomCount }]);
+    if (roomCount == 0) {
+      console.log(selectedRooms);
+      setSelectedRooms([...selectedRooms, { ...room, roomCount }]);
+    } else {
+      setSelectedRooms([...selectedRooms, { ...room, roomCount }]);
+    }
   }
 
-  const data2 = rooms.map((room: any, i: any) => ({
-    id: room.id,
-    title: room.title,
-    price: (
-      <div className="">
-        {room?.discount_price ? (
-          <>
-            <h6>
-              <del>NRs.{room?.price}</del>
-            </h6>
-            <h3>
-              NRs.
-              {room?.price - room?.discount_price}
-            </h3>
-          </>
-        ) : (
-          <h6>NRs.{room?.price}</h6>
-        )}
-      </div>
-    ),
-    sleeps: (
-      <span className="sleeping_wrapper">
-        <p>
-          <b>Full Bed </b>: {room?.full_bed}
-        </p>
-        <p>
-          <b>King Bed </b>: {room?.king_bed}
-        </p>
-        <p>
-          <b>Queen Bed </b>: {room?.queen_bed}
-        </p>
-        <p>
-          <b>Single Bed </b>: {room?.small_single_bed}
-        </p>
-        <p>
-          <b>Twin Bed </b>: {room?.twin_bed}
-        </p>
-      </span>
-    ),
-    select: (
-      <>
-        {" "}
-        <Select
-          defaultValue="0"
-          style={{ width: 120 }}
-          onChange={({ value }: any) => handleRoomChange(room, value)}
-          options={Array.from(
-            { length: room?.available_rooms },
-            (_, i) => i + 1
-          ).map((item) => {
-            return {
-              value: item,
-              label: item,
-            };
-          })}
-        />
-      </>
-    ),
-  }));
+  const data2 = rooms.map((room: any, i: any) => {
+    let totalRoomLength = Array.from(
+      { length: room?.available_rooms },
+      (_, i) => i + 1
+    );
+    totalRoomLength.splice(0, 0, 0);
+    return {
+      id: room.id,
+      title: room.title,
+      price: (
+        <div className="">
+          {room?.discount_price ? (
+            <>
+              <h6>
+                <del>NRs.{room?.price}</del>
+              </h6>
+              <h3>
+                NRs.
+                {room?.price - room?.discount_price}
+              </h3>
+            </>
+          ) : (
+            <h6>NRs.{room?.price}</h6>
+          )}
+        </div>
+      ),
+      sleeps: (
+        <span className="sleeping_wrapper">
+          <p>
+            <b>Full Bed </b>: {room?.full_bed}
+          </p>
+          <p>
+            <b>King Bed </b>: {room?.king_bed}
+          </p>
+          <p>
+            <b>Queen Bed </b>: {room?.queen_bed}
+          </p>
+          <p>
+            <b>Single Bed </b>: {room?.small_single_bed}
+          </p>
+          <p>
+            <b>Twin Bed </b>: {room?.twin_bed}
+          </p>
+        </span>
+      ),
+      select: (
+        <>
+          {" "}
+          <Select
+            defaultValue="0"
+            style={{ width: 120 }}
+            onChange={(value: any) => handleRoomChange(room, value)}
+            options={totalRoomLength.map((item) => {
+              return {
+                value: item,
+                label: item,
+              };
+            })}
+          />
+        </>
+      ),
+    };
+  });
 
   return (
     <div>
@@ -103,8 +112,9 @@ const RoomTable = ({ roomLoading, rooms }: any) => {
               title="Rooms"
               columns={columns}
               data={data2}
+              selectableRowDisabled={(row) => true}
               selectableRows
-              onSelectedRowsChange={console.log}
+              //   onSelectedRowsChange={(value: any) => console.log(value)}
               selectableRowSelected={(row) =>
                 selectedRooms.find((room: any) => room.id === row.id)
               }
