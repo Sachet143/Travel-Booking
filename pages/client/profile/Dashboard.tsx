df75b9f87b746a8fad9bcb85bd57ac2a3a503026
lokesh <lokesh@esrtech.io>
2022-12-13 23:32:20 +0545

    booking table

--- pages/client/profile/Dashboard.tsx
+++ pages/client/profile/Dashboard.tsx
@@ -1,3 +1,4 @@
+import BookingTable from "@/components/client/profile/BookingTable";
 import moment from "moment";
 import React from "react";
 import useSWR from "swr";
@@ -5,9 +6,7 @@ import useSWR from "swr";
 const Dashboard = () => {
   const { data } = useSWR("/user/booking");
 
-  function cancleHandler() {
-
-  }
+  function cancleHandler() {}
 
   return (
     <>
@@ -38,40 +37,8 @@ const Dashboard = () => {
         </div>
       </div>
       <div className="dashboard_common_table">
-        <h3>My bookings</h3>
-        <div className="table-responsive-lg table_common_area">
-          <table className="table">
-            <thead>
-              <tr>
-                <th>SL no.</th>
-                <th>Receipt No.</th>
-                <th>Number of People</th>
-                <th>Total Amount</th>
-                <th>Booking Date</th>
-                <th>Stay Date</th>
-                <th>Action</th>
-              </tr>
-            </thead>
-            <tbody>
-              {data?.data?.map((item: any, index: any) => {
-                return (
-                  <tr key={item.id}>
-                    <td>{index + 1}</td>
-                    <td>{item?.receipt_no}</td>
-                    <td>{item?.hotel?.name}</td>
-                    <td>Rs.{item.total}.00</td>
-                    <td>{moment(item.date).format("MMM Do YYYY")}</td>
-                    <td>
-                      {moment(item.from).format("L")} -&nbsp;
-                      {moment(item.to).format("L")}
-                    </td>
-                    <td><button className="btn btn-sm text-danger" onClick={cancleHandler}>Cancel</button></td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
-        </div>
+        <h3 className="mb-4">My bookings</h3>
+        <BookingTable />
       </div>
     </>
   );
