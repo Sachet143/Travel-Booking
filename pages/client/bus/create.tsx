b1572034ab7a72e4412325accf9c34cc6d361ac5
Prawesh Lamsal <prawesh.lamsal@infodevelopers.com.np>
2023-01-15 09:12:28 +0545

    Updated the seat settings

--- pages/client/bus/create.tsx
+++ pages/client/bus/create.tsx
@@ -1,6 +1,8 @@
-import React, { useState } from "react";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
 
 const Create = () => {
+  const router = useRouter();
   const seatName = ["A", "B", "C", "D", "E"];
 
   function createSeatLayout(column: any) {
@@ -16,8 +18,7 @@ const Create = () => {
     }
     return seatLayout;
   }
-  const layout = createSeatLayout(15);
-  const [removeSeat, setRemoveSeat] = useState<any>([]);
+
   function groupBy(list: any, keyGetter: any) {
     const map = new Map();
     list?.forEach((item: any) => {
@@ -31,50 +32,22 @@ const Create = () => {
     });
     return map;
   }
-  const removeSeats = (item: any) => {
-    // if (
-    //   removeSeat.filter((e: any) => e.column_name === item.column_name).length >
-    //   0
-    // ) {
-    //   const allremoveSeat = removeSeat.filter((bookItem: any) => {
-    //     return bookItem.column_name != item.column_name;
-    //   });
-    //   setRemoveSeat(allremoveSeat);
-    // } else {
-    // }
-    setRemoveSeat([...removeSeat, item]);
+
+  const [layout, setLayout] = useState(
+    groupBy(createSeatLayout(15), (item: any) => item.row_name)
+  );
+
+  const removeSeats = (seat: any) => {
+    const afterRemove = addItems(layout.get(seat.row_name), seat);
+    layout.set(seat.row_name, afterRemove);
+    setLayout(layout);
+    router.push(router.asPath);
   };
 
-  //   const availableSeats = layout.filter((item1: any) => {
-  //     return !removeSeat.some((item2: any) => {
-  //       return item2.column_name == item1.column_name;
-  //     });
-  //   });
-
-  const managedSeats = layout.map((item1: any, index: any) => {
-    if (
-      removeSeat.some((item2: any) => {
-        return item2.column_name == item1.column_name;
-      })
-    ) {
-      return {
-        row_name: item1.row_name,
-        column_name: "",
-        has_seat: item1.has_seat,
-      };
-    } else {
-      return item1;
-    }
-  });
-
-  const groupedManagedSeats = groupBy(
-    managedSeats,
-    (item: any) => item.row_name
-  );
-  function addItems(array: any) {
+  function addItems(array: any, seat: any) {
     let emptyValue = 0;
     let finalValue = array.map((item: any, index: any) => {
-      if (item.column_name == "") {
+      if (item.column_name == seat.column_name || item.column_name == "") {
         emptyValue = emptyValue + 1;
         return {
           row_name: item.row_name,
@@ -92,15 +65,6 @@ const Create = () => {
     return finalValue;
   }
 
-  const finalSeats = seatName.map((item: any) => {
-    return addItems(groupedManagedSeats.get(item));
-  });
-
-  const finalSeatManaged = groupBy(
-    finalSeats.flat(1),
-    (item: any) => item.row_name
-  );
-
   return (
     <div className="details-container">
       <div className="seat_container">
@@ -108,7 +72,7 @@ const Create = () => {
         <div className="all_seat_wrapper">
           <div className="driver">FRONT</div>
           <>
-            {Array.from(finalSeatManaged)
+            {Array.from(layout)
               .reverse()
               .map((item: any, index: any) => {
                 return (
@@ -124,14 +88,7 @@ const Create = () => {
                             <>
                               {
                                 <div
-                                  className={`custom-seat ${
-                                    removeSeat.filter(
-                                      (e: any) =>
-                                        e.column_name === item1.column_name
-                                    ).length > 0
-                                      ? "selected_seat"
-                                      : "available_seats"
-                                  }`}
+                                  className={`custom-seat available_seats`}
                                   onClick={() => removeSeats(item1)}
                                 >
                                   {item1?.column_name}
