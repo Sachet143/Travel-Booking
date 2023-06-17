337aa83df7bae890a0744311c6970ceb26ef44db
lokesh <lokesh@esrtech.io>
2023-06-18 00:32:36 +0545

    bus dashboard

---
+++ components/busadmin/dashboard/Card.tsx
@@ -0,0 +1,68 @@
+import { DatePicker, Popover } from "antd";
+import clsx from "clsx";
+import { ReactElement } from "react";
+const { RangePicker } = DatePicker;
+
+interface IProps {
+  data: number | string;
+  title: string;
+  icon?: ReactElement;
+  color:
+    | "l-bg-blue-dark"
+    | "l-bg-green-dark"
+    | "l-bg-orange-dark"
+    | "l-bg-cherry";
+  filterName?: string;
+  onChangeDate?: (name: string, val: Array<any> | null) => void;
+}
+
+function BusDashboardCard({
+  data,
+  title,
+  icon,
+  color,
+  filterName,
+  onChangeDate,
+}: IProps) {
+  return (
+    <div className="col-sm-3">
+      <div className={clsx("d-card", color)}>
+        <div className="card-statistic-3 p-4">
+          <div className="card-icon card-icon-large">{icon}</div>
+          <div className="mb-4 d-flex justify-content-between">
+            <h3 style={{ fontWeight: 700 }} className="mb-0 text-white">
+              {title}
+            </h3>
+            {filterName ? (
+              <Popover
+                trigger="click"
+                title="Choose a Date"
+                content={
+                  <RangePicker
+                    onChange={(val) =>
+                      onChangeDate && onChangeDate(filterName, val)
+                    }
+                  />
+                }
+              >
+                <i
+                  className="fa fa-ellipsis-v cursor-pointer"
+                  style={{ zIndex: 100 }}
+                />
+              </Popover>
+            ) : null}
+          </div>
+          <div className="row align-items-center mb-2 d-flex">
+            <div className="col-8">
+              <h2 className="d-flex align-items-center mb-0 text-white">
+                {data}
+              </h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default BusDashboardCard;
--- pages/busadmin/index.tsx
+++ pages/busadmin/index.tsx
@@ -1,23 +1,350 @@
-import BusadminLayout from '@/components/layout/busadmin';
-import axiosServer from '@/services/axios/serverfetch';
-import { TOKEN_KEY, USER_TYPE_KEY } from '@/services/constants';
-import { appDecrypt } from '@/services/helper';
-import { deleteCookie, getCookie } from 'cookies-next';
-import { NextPageContext } from 'next';
+import BusDashboardCard from "@/components/busadmin/dashboard/Card";
+import BusadminLayout from "@/components/layout/busadmin";
+import axiosClient from "@/services/axios/clientfetch";
+import axiosServer from "@/services/axios/serverfetch";
+import { TOKEN_KEY, USER_TYPE_KEY } from "@/services/constants";
+import {
+  appDecrypt,
+  cleanUrlParams,
+  responseErrorHandler,
+} from "@/services/helper";
+import { DatePicker, Divider, Popover, Skeleton } from "antd";
+import { deleteCookie, getCookie } from "cookies-next";
+import moment from "moment";
+import { NextPageContext } from "next";
+import { useEffect, useState } from "react";
+import { Bar } from "react-chartjs-2";
+import useSWR from "swr";
+import {
+  BarElement,
+  CategoryScale,
+  Chart as ChartJS,
+  Legend,
+  LinearScale,
+  Title,
+  Tooltip,
+} from "chart.js";
+const { RangePicker } = DatePicker;
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const options1 = {
+  indexAxis: "y" as const,
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top" as const,
+    },
+    title: {
+      display: true,
+      text: "Total Seat Booking Count",
+    },
+  },
+};
+const options2 = {
+  indexAxis: "y" as const,
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top" as const,
+    },
+    title: {
+      display: true,
+      text: "Total Trip Count",
+    },
+  },
+};
 
 function BusadminIndex() {
+  const [filterResponse, setFilterResponse] = useState<any>({
+    TotalBookingCount: null,
+    TotalRevenue: null,
+    TotalRouteSeatBookingCount: null,
+    TotalRouteTripCount: null,
+  });
+  const [filter, setFilter] = useState<any>({
+    name: "",
+    start_date: null,
+    end_date: null,
+  });
+
+  const { data: dashboard } = useSWR("/bus/dashboard");
+
+  async function fetchFilter(name: string) {
+    filter.name
+      ? await axiosClient(cleanUrlParams("/bus/reports", filter))
+          .then(({ data }: any) => {
+            setFilterResponse({
+              ...filterResponse,
+              [name]:
+                name === "TotalRouteSeatBookingCount" ||
+                name === "TotalRouteTripCount"
+                  ? data
+                  : Object.values(data)[0],
+            });
+          })
+          .catch(responseErrorHandler)
+      : null;
+  }
+
+  useEffect(() => {
+    fetchFilter(filter.name);
+  }, [filter]);
+
   return (
-    <BusadminLayout title='Dashboard'>
-      <></>
+    <BusadminLayout title="Dashboard">
+      <>
+        {!dashboard ? (
+          <Skeleton active />
+        ) : (
+          <>
+            {/* Card */}
+            <div className="d-flex gap-3">
+              {/* total booking count */}
+              <BusDashboardCard
+                title="Total Booking Count"
+                data={
+                  filterResponse.TotalBookingCount !== null
+                    ? filterResponse.TotalBookingCount
+                    : dashboard?.data?.total_bookings_count
+                }
+                icon={<i className="fas fa-bus"></i>}
+                color="l-bg-blue-dark"
+                filterName="TotalBookingCount"
+                onChangeDate={(name: string, val: Array<any> | null) =>
+                  val
+                    ? setFilter({
+                        name,
+                        start_date: moment(val[0].$d).format("YYYY-MM-DD"),
+                        end_date: moment(val[1].$d).format("YYYY-MM-DD"),
+                      })
+                    : setFilterResponse({
+                        ...filterResponse,
+                        TotalBookingCount: null,
+                      })
+                }
+              />
+              {/* total revenue */}
+              <BusDashboardCard
+                title="Total Revenue"
+                data={
+                  filterResponse.TotalRevenue !== null
+                    ? "Rs. " + filterResponse.TotalRevenue
+                    : "Rs. " + dashboard?.data?.total_revenue
+                }
+                icon={<i className="fas fa-dollar-sign"></i>}
+                color="l-bg-green-dark"
+                filterName="TotalRevenue"
+                onChangeDate={(name: string, val: Array<any> | null) =>
+                  val
+                    ? setFilter({
+                        name,
+                        start_date: moment(val[0].$d).format("YYYY-MM-DD"),
+                        end_date: moment(val[1].$d).format("YYYY-MM-DD"),
+                      })
+                    : setFilterResponse({
+                        ...filterResponse,
+                        TotalRevenue: null,
+                      })
+                }
+              />
+              {/* current month revenue */}
+              <BusDashboardCard
+                title={"Revenue " + moment().format("(MMMM)")}
+                data={"Rs. " + dashboard?.data?.current_month_revenue}
+                icon={
+                  <h1 style={{ fontWeight: 800 }}>{moment().format("MMM")}</h1>
+                }
+                color="l-bg-orange-dark"
+              />
+              {/* current_month_booking_count */}
+              <BusDashboardCard
+                title={"Booking Count " + moment().format("(MMMM)")}
+                data={dashboard?.data?.current_month_booking_count}
+                color="l-bg-cherry"
+              />
+            </div>
+            <Divider className="mt-5" />
+            {/* Charts */}
+            <div className="row justify-content-center">
+              <div className="col-sm-6">
+                <div className="w-100" style={{ textAlign: "right" }}>
+                  <Popover
+                    onOpenChange={(val) =>
+                      !val &&
+                      setFilterResponse({
+                        ...filterResponse,
+                        TotalRouteSeatBookingCount: null,
+                      })
+                    }
+                    trigger="click"
+                    title="Choose a Date"
+                    content={
+                      <RangePicker
+                        onChange={(val: any) =>
+                          val
+                            ? setFilter({
+                                name: "TotalRouteSeatBookingCount",
+                                start_date: moment(val[0].$d).format(
+                                  "YYYY-MM-DD"
+                                ),
+                                end_date: moment(val[1].$d).format(
+                                  "YYYY-MM-DD"
+                                ),
+                              })
+                            : setFilterResponse({
+                                ...filterResponse,
+                                TotalRouteSeatBookingCount: null,
+                              })
+                        }
+                      />
+                    }
+                  >
+                    <i
+                      className="fa fa-ellipsis-v cursor-pointer"
+                      style={{ zIndex: 100 }}
+                    />
+                  </Popover>
+                </div>
+                <Bar
+                  height={170}
+                  options={options1}
+                  data={{
+                    datasets: [
+                      {
+                        label: "Seat Booking Count",
+                        backgroundColor: "#8b3eea",
+                        data:
+                          filterResponse.TotalRouteSeatBookingCount !== null
+                            ? filterResponse.TotalRouteSeatBookingCount?.map(
+                                (trip: any) =>
+                                  trip.total_route_seat_booking_count
+                              )?.slice(0, 10)
+                            : dashboard?.data?.total_route_seat_booking_count
+                                ?.map(
+                                  (trip: any) =>
+                                    trip.total_route_seat_booking_count
+                                )
+                                ?.slice(0, 10),
+                      },
+                    ],
+                    labels:
+                      filterResponse.TotalRouteSeatBookingCount !== null
+                        ? filterResponse.TotalRouteSeatBookingCount?.map(
+                            (trip: any) =>
+                              trip.start_destination +
+                              " - " +
+                              trip.final_destination
+                          )?.slice(0, 10)
+                        : dashboard?.data?.total_route_seat_booking_count
+                            ?.map(
+                              (trip: any) =>
+                                trip.start_destination +
+                                " - " +
+                                trip.final_destination
+                            )
+                            ?.slice(0, 10),
+                  }}
+                />
+              </div>
+              <div className="col-sm-6">
+                <div className="w-100" style={{ textAlign: "right" }}>
+                  <Popover
+                    onOpenChange={(val) =>
+                      !val &&
+                      setFilterResponse({
+                        ...filterResponse,
+                        TotalRouteTripCount: null,
+                      })
+                    }
+                    trigger="click"
+                    title="Choose a Date"
+                    content={
+                      <RangePicker
+                        onChange={(val: any) =>
+                          val
+                            ? setFilter({
+                                name: "TotalRouteTripCount",
+                                start_date: moment(val[0].$d).format(
+                                  "YYYY-MM-DD"
+                                ),
+                                end_date: moment(val[1].$d).format(
+                                  "YYYY-MM-DD"
+                                ),
+                              })
+                            : setFilterResponse({
+                                ...filterResponse,
+                                TotalRouteTripCount: null,
+                              })
+                        }
+                      />
+                    }
+                  >
+                    <i
+                      className="fa fa-ellipsis-v cursor-pointer"
+                      style={{ zIndex: 100 }}
+                    />
+                  </Popover>
+                </div>
+                <Bar
+                  height={170}
+                  options={options2}
+                  data={{
+                    datasets: [
+                      {
+                        label: "Seat Revenue Cost",
+                        backgroundColor: "#3bad3d",
+                        data:
+                          filterResponse.TotalRouteTripCount !== null
+                            ? filterResponse.TotalRouteTripCount?.map(
+                                (trip: any) => trip.total_route_trip_count
+                              )?.slice(0, 10)
+                            : dashboard?.data?.total_route_trip_count
+                                ?.map(
+                                  (trip: any) => trip.total_route_trip_count
+                                )
+                                ?.slice(0, 10),
+                      },
+                    ],
+                    labels:
+                      filterResponse.TotalRouteTripCount !== null
+                        ? filterResponse.TotalRouteTripCount?.map(
+                            (trip: any) =>
+                              trip.start_destination +
+                              " - " +
+                              trip.final_destination
+                          )?.slice(0, 10)
+                        : dashboard?.data?.total_route_trip_count
+                            ?.map(
+                              (trip: any) =>
+                                trip.start_destination +
+                                " - " +
+                                trip.final_destination
+                            )
+                            ?.slice(0, 10),
+                  }}
+                />
+              </div>
+            </div>
+          </>
+        )}
+      </>
     </BusadminLayout>
-  )
+  );
 }
 
 export const getServerSideProps = async (ctx: NextPageContext) => {
   const { req, res } = ctx;
   const token = appDecrypt(getCookie(TOKEN_KEY, ctx) + "");
 
-  const { data: busUser } = await axiosServer(token).get('/bus-operator/user')
+  const { data: busUser } = await axiosServer(token)
+    .get("/bus-operator/user")
     .catch((err: any) => {
       console.log({ err });
 
@@ -26,22 +353,22 @@ export const getServerSideProps = async (ctx: NextPageContext) => {
 
       return {
         redirect: {
-          destination: '/client'
-        }
-      }
-    })
+          destination: "/client",
+        },
+      };
+    });
 
   if (!busUser.bus_operator_id) {
     return {
       redirect: {
-        destination: '/busadmin/bus/create'
-      }
-    }
+        destination: "/busadmin/bus/create",
+      },
+    };
   }
 
   return {
-    props: {}
-  }
-}
+    props: {},
+  };
+};
 
-export default BusadminIndex
+export default BusadminIndex;
